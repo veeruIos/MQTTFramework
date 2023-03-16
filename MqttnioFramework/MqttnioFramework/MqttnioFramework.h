827c89f5ba7bab28cf0baf333c59788bce765e92
Veerabrahmam Suthari <veerabrahmam.suthari@walmart.com>
2023-03-16 13:23:37 -0500

    MqttnioFramework

---
+++ MqttnioFramework/MqttnioFramework/MqttnioFramework.h
@@ -0,0 +1,18 @@
+//
+//  MqttnioFramework.h
+//  MqttnioFramework
+//
+//  Created by Veerabrahmam Suthari on 3/16/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MqttnioFramework.
+FOUNDATION_EXPORT double MqttnioFrameworkVersionNumber;
+
+//! Project version string for MqttnioFramework.
+FOUNDATION_EXPORT const unsigned char MqttnioFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MqttnioFramework/PublicHeader.h>
+
+
